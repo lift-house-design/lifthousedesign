ff62160bc07c09e61db39c08e403ab33eda010a1
nickniebaum <nickniebaum@gmail.com>
2013-07-16 17:11:29 -0500

    ,

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,17 +1,22 @@
 <div id="dashboard">
-	<h3>Proposals</h3>
-	<div>
-		proposal content
-	</div>
-	<h3>Projects</h3>
-	<div>
-		projects content
-	</div>
-	<h3>Account Information</h3>
-	<div>
-		acct info content
-	</div>
+	<h2 class="colorbar-1">Estimates</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed tincidunt felis. Suspendisse fringilla est arcu, 
+	quis pretium felis ullamcorper eget. Aliquam et mollis metus, eu condimentum risus. Nunc magna tortor, tempor commodo
+	sapien sit amet, consequat sollicitudin mauris. Curabitur accumsan neque arcu, vel commodo nunc eleifend sed. Integer
+	urna eros, tempus sollicitudin orci at, bibendum cursus velit.</p>
+	<h2 class="colorbar-2">Billing</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed tincidunt felis. Suspendisse fringilla est arcu, 
+	quis pretium felis ullamcorper eget. Aliquam et mollis metus, eu condimentum risus. Nunc magna tortor, tempor commodo
+	sapien sit amet, consequat sollicitudin mauris. Curabitur accumsan neque arcu, vel commodo nunc eleifend sed. Integer
+	urna eros, tempus sollicitudin orci at, bibendum cursus velit.</p>
+	<h2 class="colorbar-3">Projects</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed tincidunt felis. Suspendisse fringilla est arcu, 
+	quis pretium felis ullamcorper eget. Aliquam et mollis metus, eu condimentum risus. Nunc magna tortor, tempor commodo
+	sapien sit amet, consequat sollicitudin mauris. Curabitur accumsan neque arcu, vel commodo nunc eleifend sed. Integer
+	urna eros, tempus sollicitudin orci at, bibendum cursus velit.</p>
+	<h2 class="colorbar-4">Admin</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed tincidunt felis. Suspendisse fringilla est arcu, 
+	quis pretium felis ullamcorper eget. Aliquam et mollis metus, eu condimentum risus. Nunc magna tortor, tempor commodo
+	sapien sit amet, consequat sollicitudin mauris. Curabitur accumsan neque arcu, vel commodo nunc eleifend sed. Integer
+	urna eros, tempus sollicitudin orci at, bibendum cursus velit.</p>
 </div>
-<script>
-	
-</script>
--- assets/js/dashboard-index.js
+++ assets/js/dashboard-index.js
@@ -1,3 +1,3 @@
 $(function(){
-	$('#dashboard').accordion();
+	
 });
