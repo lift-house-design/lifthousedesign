f5d840cdfe2d003210b755b581983d8c92636daf
nickniebaum <nickniebaum@gmail.com>
2013-08-16 10:09:30 -0500

    Updated terms and conditions and privacy policy

--- application/views/site/pages/terms_conditions.php
+++ application/views/site/pages/terms_conditions.php
@@ -1,18 +1,18 @@
 <h1>Terms and Conditions</h1>
 <p>By working with Lift House Design, you confirm that you are in agreement with and bound by the terms and conditions below.</p>
 <h3>Definitions</h3>
-<p><strong>The Client</strong>: The company or individual requesting the services of Lift House Design.</p>
-<p><strong>Solve Web Design</strong>: Primary designer/site owner & employees or affiliates.</p>
+<p><em>The Client</em>: The company or individual requesting the services of Lift House Design.<br />
+<em>Solve Web Design</em>: Primary designer/site owner &amp; employees or affiliates.</p>
 <h3>General</h3>
 <p>Lift House Design will carry out work only where an agreement is provided either by email, telephone, mail or fax. Lift House Design will carry out work only for clients who are 18 years of age or above. An 'order' is deemed to be a written or verbal contract between Lift House Design and the client, this includes telephone and email agreements.</p>
 <h3>Website Design</h3>
 <p>Whilst every endeavour will be made to ensure that the website and any scripts or programs are free of errors, Lift House Design cannot accept responsibility for any losses incurred due to malfunction, the website or any part of it.</p>
-<p>The website, graphics and any programming code remain the property of Lift House Design until all outstanding accounts are paid in full. </p>
+<p>The website, graphics and any programming code remain the property of Lift House Design until all outstanding accounts are paid in full.</p>
 <p>Any scripts, cgi applications, php scripts, or software (unless specifically agreed) written by Lift House Design remain the copyright of Lift House Design and may only be commercially reproduced or resold with the permission of Lift House Design.</p>
-<p>Lift House Design cannot take responsibility for any copyright infringements caused by materials submitted by the client. We reserve the right to refuse any material of a copyrighted nature unless adequate proof is given of permission to use such material.
-Any additions to the brief will be carried out at the discretion of Lift House Design and where no charge is made by Lift House Design for such additions, Lift House Design accept no responsibility to ensure such additions are error free and reserve the right to charge an according amount for any correction to these or further additions.</p>
+<p>Lift House Design cannot take responsibility for any copyright infringements caused by materials submitted by the client. We reserve the right to refuse any material of a copyrighted nature unless adequate proof is given of permission to use such material.</p>
+<p>Any additions to the brief will be carried out at the discretion of Lift House Design and where no charge is made by Lift House Design for such additions, Lift House Design accept no responsibility to ensure such additions are error free and reserve the right to charge an according amount for any correction to these or further additions.</p>
 <p>The client agrees to make available as soon as is reasonably possible to Lift House Design all materials required to complete the site to the agreed standard and within the set deadline.</p>
-<p>Lift House Design will not be liable for costs incurred, compensation or loss of earnings due to the failure to meet agreed deadlines. </p>
+<p>Lift House Design will not be liable for costs incurred, compensation or loss of earnings due to the failure to meet agreed deadlines.</p>
 <p>Lift House Design will not be liable or become involved in any disputes between the site owner and their clients and cannot be held responsible for any wrongdoing on the part of a site owner.</p>
 <p>Lift House Design will not be liable for any costs incurred, compensation or loss of earnings due to the work carried out on behalf of the client or any of the clients appointed agents.</p>
 <p>Lift House Design will not be liable for any costs incurred, compensation or loss of earnings due to the unavailability of the site, its servers, software or any material provided by its agents.</p>
@@ -21,8 +21,8 @@ Any additions to the brief will be carried out at the discretion of Lift House D
 <h3>Database, Application and E-Commerce Development</h3>
 <p>Lift House Design cannot take responsibility for any losses incurred by the use of any software created for the client. Whilst every care has been taken to ensure products are problem free and accurate, the ultimate responsibility lies with the client in ensuring that all software is functioning correctly before use.</p>
 <p>The client is expected to test fully any application or programming relating to a site developed by Lift House Design before being made generally available for use. Where "bugs", errors or other issues are found after the site is live, Lift House Design will endeavour (but is not obliged to) to correct these issues to meet the standards of function outlined in the brief.</p>
-<h3>Compatibility </h3>
-<p>Lift House Design will endeavour to ensure that any developed/designed site or application will function correctly on the server it is initially installed in and that it will function correctly when viewed with the web browsing software Microsoft Internet Explorer Version 6 and to an acceptable level with Mozilla browsers. Lift House Design can offer no guarantees of correct function with all browser software.</p>
+<h3>Compatibility</h3>
+<p>Lift House Design will endeavour to ensure that any developed/designed site or application will function correctly on the server it is initially installed in and that it will function correctly when viewed with current versions of web browsing software. Lift House Design can offer no guarantees of correct function with outdated browser software.</p>
 <h3>Website Hosting</h3>
 <p>Whilst Lift House Design recommends hosting companies to host websites, no guarantees can be made as to the availability or interruption of this service by Lift House Design cannot accept liability for losses caused by the unavailability, malfunction or interruption of this service, or for loss of turnover, sales, revenue, profits or indirect, consequential or special loss.</p>
 <p>Lift House Design reserves the right to refuse to handle in any way, material which may be deemed offensive, illegal or in any way controversial, and also to terminate the hosting service should the necessity arise.</p>
@@ -33,9 +33,9 @@ Any additions to the brief will be carried out at the discretion of Lift House D
 <p>Following consistent non payment of an invoice our Solicitors will contact the client in question, with a view to taking the matter further and if need be to seek payment through legal procedures, and if necessary court summons.</p>
 <h3>Complaints Procedure</h3>
 <h4>Informal procedure</h4>
-<p>Anyone who experiences a problem with their web service provided by Lift House Design should raise the matter directly using our online contact form to do so, giving sufficient information to locate the material (such as an url) and clearly outlining the grounds for complaint. </p>
+<p>Anyone who experiences a problem with their web service provided by Lift House Design should raise the matter directly using our online contact form to do so, giving sufficient information to locate the material (such as a url) and clearly outlining the grounds for complaint.</p>
 <p>Lift House Design will approach the individual responsible for the material in question with a view to resolving the matter to the satisfaction of the complainant.</p>
 <h4>Formal complaints procedure</h4>
-<p>The formal complaints procedure should only be used where the complainant feels that the nature of the complaint is too serious to be dealt with informally, or where a sat	isfactory conclusion has not been reached after following the informal procedure. 
+<p>The formal complaints procedure should only be used where the complainant feels that the nature of the complaint is too serious to be dealt with informally, or where a satisfactory conclusion has not been reached after following the informal procedure.</p>
 <p>A formal complaint should be made in writing to Solve Web Design, who will acknowledge receipt and ensure that the matter is looked into as soon as possible.</p>
 <p>An initial response to any complaint can be expected within seven days of its receipt; a full and considered response to the complaint should be completed within 30 days and any subsequent remedy implemented with the minimum of delay.</p>
