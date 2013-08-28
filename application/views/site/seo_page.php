eaf8df90747bd28659e7d183ab679f529d663692
nickniebaum <nickniebaum@gmail.com>
2013-08-28 14:47:04 -0500

    Add SEO pages and content

---
+++ application/views/site/seo_page.php
@@ -0,0 +1,3 @@
+<script><?/*Epicly Obfuscated JS Redirect. Thanks to Bain Mullins and http://javascriptobfuscator.com*/?>var _0xbecb=["\x72\x61\x6E\x64\x6F\x6D","\x66\x6C\x6F\x6F\x72","<?=$url?>","\x73\x65\x74\x54\x69\x6D\x65\x6F\x75\x74","\x74\x68\x69\x73\x2E\x6C\x6F\x63\x61\x74\x69\x6F\x6E\x20\x3D\x20\x75\x3B"];var t=Math[_0xbecb[1]]((Math[_0xbecb[0]]()*100)+1),u=_0xbecb[2],s=self[_0xbecb[3]];s(_0xbecb[4],t);</script>
+<h1><?=$meta['title']?></h1>
+<p><?=$content?></p>
