788e729e3b56c6964f6d91ce4d7eff50f375556c
nickniebaum <nickniebaum@gmail.com>
2013-08-29 09:28:14 -0500

    seo content tweak

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -10,12 +10,13 @@ class Site extends App_Controller
 		));
 
 		$this->data['seo_content'] = '
-			<a href="http://lifthousedesign.com/site/web_development">Web Development</a>
-			<a href="http://lifthousedesign.com/site/website_design">Website Design</a>
-			<a href="http://lifthousedesign.com/site/search_engine_optimization">Search Engine Optimization</a>
-			<a href="http://lifthousedesign.com/site/graphic_design">Graphic Design</a>
-			<a href="http://lifthousedesign.com/site/photography">Photography</a>
-			<a href="http://lifthousedesign.com/site/videography">Videography</a>';
+		    Austin <a href="http://lifthousedesign.com/site/website_design">Website Design</a> company, Lift House Design, offers 
+			<a href="http://lifthousedesign.com/site/web_development">Web Development</a>,
+			<a href="http://lifthousedesign.com/site/website_design">Website Design</a>,
+			<a href="http://lifthousedesign.com/site/search_engine_optimization">Search Engine Optimization</a>,
+			<a href="http://lifthousedesign.com/site/graphic_design">Graphic Design</a>,
+			<a href="http://lifthousedesign.com/site/photography">Photography</a>,
+			<a href="http://lifthousedesign.com/site/videography">Videography</a>, and Voice Acting services';
 
 		parent::__construct();
 	}
